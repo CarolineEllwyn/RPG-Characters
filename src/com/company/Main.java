7ff9988f21f108ca7b3d727408b273dafa582179
Caroline Ellwyn <caroline.ellwyn@se.experis.com>
2020-10-12 15:44:04 +0200

    create interface for heros. create different kinds of heroes, warrior, ranger and mage.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,21 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
-
-       // Warrior warrior = new Warrior();
-      //  System.out.println("level 1: " +  "Dex: "+ warrior.getDexterity() + " HP: " + warrior.getHealth() + " Level: " + warrior.getLevel() + " Str: " + warrior.getStrength());
-       // warrior.onLevelUp();
-       // System.out.println("level 2: " +  "Dex: "+ warrior.getDexterity() + " HP: " + warrior.getHealth() + " Level: " + warrior.getLevel() + " Str: " + warrior.getStrength());
-      //  warrior.onLevelUp();
-      //  System.out.println("level 3: " +  "Dex: "+ warrior.getDexterity() + " HP: " + warrior.getHealth() + " Level: " + warrior.getLevel() + " Str: " + warrior.getStrength());
-
-
-       // System.out.println(warrior.getLevel());
-       // System.out.println(warrior.getPointsToNextLevel(6));
-       // feeeeeeel uträkning warrior.gainExperience(100);
-
+        
         //System.out.println(warrior.getExperienceTargetByLevel(6));
 
         Warrior myWarrior = new Warrior();
